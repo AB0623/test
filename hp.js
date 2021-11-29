aeabfb358f8f5655ab2f552c2c424e5e1f63d00a
AB0623 <93128483+AB0623@users.noreply.github.com>
2021-11-29 16:37:58 +0800

    Update hp.js

--- hp.js
+++ hp.js
@@ -1,1 +1,20 @@
 
+window.onload=function(){
+    cvs=document.getElementById("cvs");
+    ctx=cvs.getContext("2d");
+    var img=new Image();
+    img.src="mm.jpg";
+    img.onload=function(){
+    ctx.drawImage(this, 0, 0, cvs.width, cvs.height);
+    };    
+}
+
+function invertColor(){
+    var pixels=ctx.getImageData(0, 0, cvs.width, cvs.height);
+    for (var i=0; i<data.length; i+=4){
+        data[i]=255-data[i];
+        data[i+1]=255-data[i+1];
+        data[i+2]=255-data[i+2];
+    };
+    ctx.putImageData(pixels, 0, 0);
+}
