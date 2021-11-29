d3193a8b7932cd06e742a98c1a8d42532d728541
AB0623 <93128483+AB0623@users.noreply.github.com>
2021-11-29 16:52:52 +0800

    Update hp.js

--- hp.js
+++ hp.js
@@ -1,7 +1,7 @@
 
 window.onload=function(){
-    cvs=document.getElementById("cvs");
-    ctx=cvs.getContext("2d");
+    var cvs=document.getElementById("cvs");
+    var ctx=cvs.getContext("2d");
     var img=new Image();
     img.src="mm.jpg";
     img.onload=function(){
