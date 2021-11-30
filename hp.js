2f858758a4d41813015e1793324b33840f162a4e
AB0623 <93128483+AB0623@users.noreply.github.com>
2021-11-30 16:46:06 +0800

    Update hp.js

--- hp.js
+++ hp.js
@@ -1,11 +1,32 @@
-window.onload = function(){dCC(); loadFile(input); invertColor(); saveFile();}
+window.onload = function(){takeVideo(); updateTimer(); playVideo(); pauseVideo(); replayVideo(); dCC(); loadFile(input); invertColor(); saveFile();}
+
+function takeVideo(){
+    var video=document.getElementById("video");
+    video.addEventListener("timeupdate", updateTimer);
+    video.addEventListener("ended", function(){
+        alert("影片結束");
+    });
+}
+function updateTimer(){
+    var timer=document.getElementById("timer");
+    timer.innerHTML=video.currentTime +"/"+ video.duration;
+}
+function playVideo(){
+    video.play();
+}
+function pauseVideo(){
+    video.pause();
+}
+function replayVideo(){
+    video.currentTime=0;
+    video.play();
+}
 
 function dCC(){
     var cvs=document.getElementById("cvs");
     ctx=cvs.getContext("2d");
     var ctx;
 }
-
 function loadFile(input){
     var file=input.files[0];
     var src=URL.createObjectURL(file);
@@ -15,7 +36,6 @@ function loadFile(input){
         ctx.drawImage(this, 0, 0, cvs.width, cvs.height);
     };
 }
-
 function invertColor(){
     var pixels=ctx.getImageData(0, 0, cvs.width, cvs.height);
     var data=pixels.data;
@@ -26,7 +46,6 @@ function invertColor(){
     };
     ctx.putImageData(pixels, 0, 0);
 }
-
 function saveFile(){
     var link=document.getElementById("download");
     link.download="image.jpg";
